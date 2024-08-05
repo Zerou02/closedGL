bf65bedddf70d3ac88b7b3d1b47934bc67b67898
Zerou <e.bertgen@gmx.de>
2024-08-05 10:39:37 +0200

    better face culling

--- ynnebcraft/Chunk.go
+++ ynnebcraft/Chunk.go
@@ -1,8 +1,6 @@
 package ynnebcraft
 
 import (
-	"strconv"
-
 	"github.com/EngoEngine/glm"
 	"github.com/Zerou02/closedGL/closedGL"
 )
@@ -10,7 +8,7 @@ import (
 type Chunk struct {
 	origin, size glm.Vec3
 	ctx          *closedGL.ClosedGLContext
-	//little-endian: ,1bit vis,6bit faceMask(little oben,vorne,...)
+	//little-endian: ,1bit transparency,6bit faceMask(little oben,vorne,...)
 	cubes []uint16
 	mesh  closedGL.CubeMesh
 }
@@ -19,16 +17,16 @@ func NewChunk(origin, size glm.Vec3, ctx *closedGL.ClosedGLContext) Chunk {
 	var amountCubes = int(size[0] * size[1] * size[2])
 	var cubeArr = make([]uint16, amountCubes)
 	var ret = Chunk{origin: origin, size: size, ctx: ctx, cubes: cubeArr}
-	for i := 0; i < len(cubeArr); i++ {
-		if ret.isInnerBlock(i) {
-			cubeArr[i] = 0
-		} else {
-			cubeArr[i] = 1
-		}
-	}
+
+	ret.setTransparency(0, true)
+	ret.setTransparency(1, true)
+	ret.setTransparency(2, true)
+	ret.setTransparency(3, true)
+	ret.setTransparency(4, true)
+	ret.setTransparency(closedGL.Pos3ToIdx(1, 1, 1, int(size[0]), int(size[1]), int(size[2])), true)
+	ret.setTransparency(closedGL.Pos3ToIdx(2, 2, 2, int(size[0]), int(size[1]), int(size[2])), true)
 	ret.faceCullCubes()
 	ret.CreateMesh()
-	println(strconv.FormatInt(int64(ret.cubes[0]), 2))
 	return ret
 }
 
@@ -36,7 +34,7 @@ func (this *Chunk) CreateMesh() {
 	this.ctx.InitCubeMesh(1)
 	for i := 0; i < len(this.cubes); i++ {
 		var c = this.cubes[i]
-		if (c>>6)&1 == 1 {
+		if !this.isTransparent(c) {
 			var x, y, z = closedGL.IdxToPos3(i, int(this.size[0]), int(this.size[1]), int(this.size[2]))
 			var faceMask = c & 63
 			for j := 0; j < 6; j++ {
@@ -54,42 +52,22 @@ func (this *Chunk) Draw() {
 	this.ctx.DrawCubeMesh(&this.mesh, 1)
 }
 
-// surrounded on all sides
-func (this *Chunk) isInnerBlock(idx int) bool {
-	var neighbours = this.getAmountNeighbours(idx)
-	var posX, posY, posZ = closedGL.IdxToPos3(idx, int(this.size[0]), int(this.size[1]), int(this.size[2]))
-
-	var isInner = posX > 0 && posX < int(this.size[0])-1 && posY > 0 && posY < int(this.size[1])-1 && posZ > 0 && posZ < int(this.size[2])-1
-	return isInner && neighbours >= 4
+func (this *Chunk) isTransparent(cube uint16) bool {
+	return (cube>>6)&1 == 1
 }
 
-// No Diagonal Neighbours
-func (this *Chunk) getAmountNeighbours(idx int) int {
-	var retAmount = 0
-	var offsets = []int{
-		-1, 0, 0,
-		1, 0, 0,
-		0, 1, 0,
-		0, -1, 0,
-		0, 0, 1,
-		0, 0, -1,
+func (this *Chunk) setTransparency(idx int, val bool) {
+	var a uint16 = 1
+	if !val {
+		a = 0
 	}
-
-	var posX, posY, posZ = closedGL.IdxToPos3(idx, int(this.size[0]), int(this.size[1]), int(this.size[2]))
-
-	for i := 0; i < len(offsets); i += 3 {
-		var newX, newY, newZ = posX + offsets[i], posY + offsets[i+1], posZ + offsets[i+2]
-
-		var idx = closedGL.Pos3ToIdx(int(newX), int(newY), int(newZ), int(this.size[0]), int(this.size[1]), int(this.size[2]))
-		if idx >= 0 && idx < len(this.cubes) {
-			retAmount += 1
-		}
-	}
-	return retAmount
+	this.cubes[idx] |= a << 6
 }
-
 func (this *Chunk) faceCullCubes() {
 	for i := 0; i < len(this.cubes); i++ {
+		if this.isTransparent(this.cubes[i]) {
+			continue
+		}
 		var dimX = int(this.size[0])
 		var dimY = int(this.size[1])
 		var dimZ = int(this.size[2])
@@ -109,8 +87,13 @@ func (this *Chunk) faceCullCubes() {
 			var isOuter = (newX < 0 || newX >= int(this.size[0])) || (newY < 0 || newY >= int(this.size[1])) || (newZ < 0 || newZ >= int(this.size[2]))
 			var newIdx = closedGL.Pos3ToIdx(newX, newY, newZ, int(this.size[0]), int(this.size[1]), int(this.size[2]))
 			if isOuter {
-				_ = newIdx
 				allowedFaceMask |= uint16(1) << (i / 3)
+			} else {
+				_ = newIdx
+				var c = this.cubes[newIdx]
+				if this.isTransparent(c) {
+					allowedFaceMask |= uint16(1) << (i / 3)
+				}
 			}
 		}
 		this.cubes[i] <<= 6
